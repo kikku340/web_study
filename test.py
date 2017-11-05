b7aa8911b14e145b672f00dcc3593a2043c54f64
Yuki Kikuchi <kikku650@gmail.com>
2017-11-05 09:38:52 +0900

    btcの価格取得

--- test.py
+++ test.py
@@ -6,12 +6,13 @@ import time
 from datetime import datetime
 
 value_url = "https://c-cex.com/t/prices.json"
-value_fname = ""
+btc_url = "https://bitflyer.jp/api/echo/price"
+
 headers = {
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0",
         }
 
-def get_price():
+def get_price_zeny():
     request = urllib.request.Request(url=value_url, headers=headers)
     response = urllib.request.urlopen(request)
     html = response.read().decode('utf-8')
@@ -20,7 +21,16 @@ def get_price():
     print(datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
     print("1bitzeny = {0}satoshi\n".format(int(float(str1) * 100000000)))
 
-while True:
+def get_price_btc():
+    request = urllib.request.Request(url=btc_url, headers=headers)
+    response = urllib.request.urlopen(request)
+    html = response.read().decode('utf-8')
+    ret = json.loads(html)
+    str1 = ret['mid']
+    print(datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+    print("1btc = {0}yen\n".format(int(float(str1))))
 
-    get_price()
+while True:
+    #get_price_zeny()
+    get_price_btc()
     time.sleep(60)
